285930deb2ecc2151f3160de4d2781885cac0ab3
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-23 22:01:29 +0100

    added NotFound result

--- Nidikwa.Service/ControllerResults.cs
+++ Nidikwa.Service/ControllerResults.cs
@@ -1,7 +1,10 @@
-﻿namespace Nidikwa.Service;
+﻿using Nidikwa.Service.Utilities;
+
+namespace Nidikwa.Service;
 
 internal partial class Controller
 {
+    private static Result NotFound(string? message = null) => new Result { Code = ResultCodes.NotFound, ErrorMessage = message };
     private static Result InvalidInputStructure(string? message = null) => new Result { Code = ResultCodes.InvalidInputStructure, ErrorMessage = message };
     private static Result InvalidEndpoint(string? message = null) => new Result { Code = ResultCodes.InvalidEndpoint, ErrorMessage = message };
     private static Result Success() => new Result { Code = ResultCodes.Success };
