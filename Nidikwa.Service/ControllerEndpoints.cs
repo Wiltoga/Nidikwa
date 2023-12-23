96d110ed5486e519c6f7600107f6d4565947975f
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-23 22:02:01 +0100

    added some endpoints

---
+++ Nidikwa.Service/ControllerEndpoints.cs
@@ -0,0 +1,25 @@
+﻿using Nidikwa.Service.Utilities;
+
+namespace Nidikwa.Service;
+
+internal partial class Controller
+{
+    [Endpoint(RouteEndpoints.GetDevices)]
+    public async Task<Result> GetAvailableDevices()
+    {
+        return Success(await audioService.GetAvailableDevicesAsync());
+    }
+
+    [Endpoint(RouteEndpoints.StartRecording)]
+    public async Task<Result> StartRecording(string deviceId)
+    {
+        await audioService.StartRecordAsync(deviceId);
+        return Success();
+    }
+
+    [Endpoint(RouteEndpoints.FindDevice)]
+    public async Task<Result> FindDevice(string deviceId)
+    {
+        return Success(await audioService.GetDeviceAsync(deviceId));
+    }
+}
