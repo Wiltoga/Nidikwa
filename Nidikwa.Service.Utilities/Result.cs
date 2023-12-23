1bf4c5e169a8072f1dd7d31ef8d55e83a4e1d0c5
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-23 22:00:06 +0100

    moved Resut to Utilities

---
+++ Nidikwa.Service.Utilities/Result.cs
@@ -0,0 +1,20 @@
+﻿namespace Nidikwa.Service.Utilities;
+
+public enum ResultCodes
+{
+    Success,
+    InvalidEndpoint,
+    InvalidInputStructure,
+    NotFound,
+}
+
+public class Result
+{
+    public ResultCodes Code { get; init; }
+    public string? ErrorMessage { get; init; }
+}
+
+public class Result<T> : Result
+{
+    public T? Data { get; init; }
+}
