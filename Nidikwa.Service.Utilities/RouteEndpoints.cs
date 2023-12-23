6c1e77276b38ed7428c1c07794df2ba7cab69160
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-23 21:59:09 +0100

    added all planned routes

---
+++ Nidikwa.Service.Utilities/RouteEndpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace Nidikwa.Service.Utilities;
+
+public static class RouteEndpoints
+{
+    public const string GetDevices = "get-devices";
+    public const string StartRecording = "start-recording";
+    public const string FindDevice = "find-device";
+    public const string WaitUntilStart = "wait-until-start";
+    public const string WaitUntilStop = "wait-until-stop";
+    public const string WaitUntilRecordAddedToQueue = "wait-until-recorded";
+    public const string WaitUntilRecordDeletedFromQueue = "wait-until-deleted";
+    public const string AddToQueue = "add-to-queue";
+    public const string GetQueueItems = "get-queue-items";
+    public const string GetQueueItem = "get-queue-item";
+    public const string DeleteQueueItem = "delete-queue-item";
+}
