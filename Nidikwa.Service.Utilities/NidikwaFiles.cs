c42027ebc325bd67e7e19b949aefbabfa3e95358
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-26 21:02:17 +0100

    added access to queue

---
+++ Nidikwa.Service.Utilities/NidikwaFiles.cs
@@ -0,0 +1,16 @@
+﻿namespace Nidikwa.Service.Utilities;
+
+public static class NidikwaFiles
+{
+    private const string NidikwaFolderName = "Nidikwa";
+    private const string QueueFolderName = "Queued";
+    public static string QueueFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), NidikwaFolderName, QueueFolderName);
+
+    public static void EnsureFolderExists()
+    {
+        if (!Directory.Exists(NidikwaFiles.QueueFolder))
+        {
+            Directory.CreateDirectory(NidikwaFiles.QueueFolder);
+        }
+    }
+}
---
+++ Nidikwa.Service.Utilities/RecordSessionFile.cs
@@ -0,0 +1,5 @@
+﻿using Nidikwa.Models;
+
+namespace Nidikwa.Service.Utilities;
+
+public record RecordSessionFile(RecordSessionMetadata SessionMetadata, FileInfo File);
