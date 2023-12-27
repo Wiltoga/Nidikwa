5d1c5930875abbef1253e6bd9a032e70f2066f08
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-27 09:57:56 +0100

    new routes values

--- Nidikwa.Service.Utilities/RouteEndpoints.cs
+++ Nidikwa.Service.Utilities/RouteEndpoints.cs
@@ -8,10 +8,9 @@ public static class RouteEndpoints
     public const string FindDevice = "find-device";
     public const string WaitUntilStart = "wait-until-start";
     public const string WaitUntilStop = "wait-until-stop";
-    public const string WaitUntilRecordAddedToQueue = "wait-until-recorded";
-    public const string WaitUntilRecordDeletedFromQueue = "wait-until-deleted";
     public const string AddToQueue = "add-to-queue";
-    public const string GetQueueItems = "get-queue-items";
-    public const string GetQueueItem = "get-queue-item";
-    public const string DeleteQueueItem = "delete-queue-item";
+    public const string DeleteFromQueue = "delete-from-queue";
+    public const string EventStatusChanged = "event-status-changed";
+    public const string EventQueueChanged = "event-queue-changed";
+    public const string EventDevicesChanged = "event-devices-changed";
 }
