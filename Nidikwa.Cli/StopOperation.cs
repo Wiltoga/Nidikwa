05910d6087d3036e889b4125f738c0bab85998b9
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-29 15:29:22 +0100

    added stop endpoint

---
+++ Nidikwa.Cli/StopOperation.cs
@@ -0,0 +1,19 @@
+﻿namespace Nidikwa.Cli;
+
+[Operation("stop", "s", "Stops the current recording")]
+internal class StopOperation : IOperation
+{
+    public async Task ExecuteAsync(string[] args)
+    {
+        if (args.Length <= 1)
+        {
+            Console.WriteLine($"""
+                Usage : stop
+                """);
+            return;
+        }
+        var instance = await SdkHandler.GetInstanceAsync();
+
+        await instance.StopRecordingAsync();
+    }
+}
