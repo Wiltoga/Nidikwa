1314f2fbb69cfaea3a50cd9fd8b6f0edbd558eba
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-29 15:33:29 +0100

    added status endpoint

---
+++ Nidikwa.Cli/StatusOperation.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Nidikwa.Common;
+
+namespace Nidikwa.Cli;
+
+[Operation("status", "ss", "Returns the current status of the service")]
+internal class StatusOperation : IOperation
+{
+    public async Task ExecuteAsync(string[] args)
+    {
+        var instance = await SdkHandler.GetInstanceAsync();
+
+        Console.Write(JsonConvert.SerializeObject(await instance.GetStatusAsync()));
+    }
+}
