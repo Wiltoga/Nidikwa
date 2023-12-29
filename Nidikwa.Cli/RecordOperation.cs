67849d948dfcd0180723a27b917730e1675c16a6
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-29 15:26:07 +0100

    added record endpoint

---
+++ Nidikwa.Cli/RecordOperation.cs
@@ -0,0 +1,23 @@
+﻿using Nidikwa.Common;
+
+namespace Nidikwa.Cli;
+
+[Operation("record", "r", "Starts recording using the given deviceIds as parameters")]
+internal class RecordOperation : IOperation
+{
+    public async Task ExecuteAsync(string[] args)
+    {
+        if (args.Length <= 1)
+        {
+            Console.WriteLine($"""
+                Usage : record <duration> [<deviceIds>]
+                Example : record 00:05:00 {Guid.Empty:B}.{Guid.NewGuid():B}
+                """);
+            return;
+        }
+        var duration = TimeSpan.Parse(args[0]);
+        var instance = await SdkHandler.GetInstanceAsync();
+
+        await instance.StartRecordingAsync(new RecordParams(args.Skip(1).ToArray(), duration));
+    }
+}
---
+++ Nidikwa.Cli/SdkHandler.cs
@@ -0,0 +1,10 @@
+﻿using Nidikwa.Sdk;
+
+namespace Nidikwa.Cli;
+
+internal static class SdkHandler
+{
+    private static IControllerService? instance;
+
+    public static async Task<IControllerService> GetInstanceAsync() => instance ?? await ControllerService.ConnectAsync();
+}
