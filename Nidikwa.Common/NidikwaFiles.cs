8295e5781e20c45cff3e79de05d7115ae8d9fffd
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-29 21:51:35 +0100

    queue items are in Wiltoga folder

--- Nidikwa.Common/NidikwaFiles.cs
+++ Nidikwa.Common/NidikwaFiles.cs
@@ -2,9 +2,10 @@
 
 public static class NidikwaFiles
 {
+    private const string WiltogaFolderName = "Wiltoga";
     private const string NidikwaFolderName = "Nidikwa";
     private const string QueueFolderName = "Queued";
-    public static string QueueFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), NidikwaFolderName, QueueFolderName);
+    public static string QueueFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), WiltogaFolderName, NidikwaFolderName, QueueFolderName);
 
     public static void EnsureQueueFolderExists()
     {
