fa63a9310cbadb00159ac53e784f0e416c401c1c
Nathan HANEN <hanen.nathan@gmail.com>
2023-12-29 15:27:36 +0100

    added queue endpoint

---
+++ Nidikwa.Cli/QueueOperation.cs
@@ -0,0 +1,19 @@
+﻿namespace Nidikwa.Cli;
+
+[Operation("queue", "q", "Enqueues the current recording")]
+internal class QueueOperation : IOperation
+{
+    public async Task ExecuteAsync(string[] args)
+    {
+        if (args.Length <= 1)
+        {
+            Console.WriteLine($"""
+                Usage : queue
+                """);
+            return;
+        }
+        var instance = await SdkHandler.GetInstanceAsync();
+
+        await instance.AddToQueueAsync();
+    }
+}
